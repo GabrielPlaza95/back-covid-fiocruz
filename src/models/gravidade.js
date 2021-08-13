56c29db8647b19c4c236bd24064510c29760acf4
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-13 00:12:22 -0300

    add new parameters to query

--- src/models/gravidade.js
+++ src/models/gravidade.js
@@ -4,7 +4,7 @@ const query = await readFile('src/queries/general_list.sql', { encoding: 'utf8' 
 const gravidade_table = "gravidade";
 
 export default async (conn) => {
-	const [rows] = await conn.execute(query, [gravidade_table])
+	const [rows] = await conn.query(query, [gravidade_table])
 
 	return rows
 }
