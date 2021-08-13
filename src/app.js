fb8cfce2eccd9eb62b6a7d7b6f598e48bdfd9b86
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-12 23:38:34 -0300

    allow cors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@ const port = process.env.PORT
 app.set('db connection pool', pool)
 app.use(express.json())
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 app.use('/', routes)
 
 app.use((req, res, next) => {
