fb247f098fc4571450e0ee8c1c15f2eb0172ad0e
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-13 00:35:44 -0300

    add new version general query

--- src/models/gravidade.js
+++ src/models/gravidade.js
@@ -4,7 +4,7 @@ import { list_query } from '../queries/general_queries';
 const gravidade_table = "gravidade";
 
 export default async (conn) => {
-	const [rows] = await conn.execute(list_query(gravidade_table))
+	const [rows] = await conn.query(list_query(gravidade_table))
 
 	return rows
 }
