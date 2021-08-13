fbec90fdee8a902783f4c2fcd6c592d9948efb13
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-13 00:40:05 -0300

    test3

--- src/models/gravidade.js
+++ src/models/gravidade.js
@@ -4,7 +4,7 @@ import { readFile } from 'fs/promises'
 const gravidade_table = "gravidade";
 
 export default async (conn) => {
-	const [rows] = await conn.query(`SELECT * FROM '${gravidade_table}';`)
+	const [rows] = await conn.query(`SELECT * FROM gravidade;`)
 
 	return rows
 }
