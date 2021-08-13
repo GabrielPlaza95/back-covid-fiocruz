580be2a20367be1772aeb85c4f7fc49ae9933dbe
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-13 00:37:42 -0300

    test

--- src/models/gravidade.js
+++ src/models/gravidade.js
@@ -1,10 +1,11 @@
 import { readFile } from 'fs/promises'
-import { list_query } from '../queries/general_queries';
+// import { list_query } from '../queries/general_queries';
 
 const gravidade_table = "gravidade";
 
 export default async (conn) => {
-	const [rows] = await conn.query(list_query(gravidade_table))
+	// const [rows] = await conn.query(list_query(gravidade_table))
+	const [rows] = [{}]
 
 	return rows
 }
