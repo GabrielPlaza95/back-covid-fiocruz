d827eda1a9f1741490e9432b67f637cb6873a837
hugo-espinelli <hugoespinelli@gmail.com>
2021-08-12 23:26:08 -0300

    change amostra route

--- src/routes/amostra.js
+++ src/routes/amostra.js
@@ -3,6 +3,17 @@ import { add, remove } from '../models/amostra.js'
 
 const router = Router()
 
+router.get('/', async (req, res) => {
+	const { busca } = req.query
+	const pool = req.app.get('db connection pool')
+
+	let term = busca ? busca : ""
+
+	const samples = await search(pool, term) 
+	res.json(samples)
+})
+
+
 router.post('/', async (req, res) => {
 	const pool = req.app.get('db connection pool')
 
